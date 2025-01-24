d2d0371b0378d9e4734a4eac2d6b5793bd241155
alex tambo <alex.tambo.15432@gmail.com>
2025-01-24 07:02:36 +0100

    Minor updates

--- push_test.sh
+++ push_test.sh
@@ -1,1 +1,1 @@
-clear && make re && valgrind --leak-check=full --show-leak-kinds=all --suppressions=./push_rl.supp --trace-children=yes --track-origins=yes --track-fds=yes -s ./minishell
+clear && make re && clear && valgrind --leak-check=full --show-leak-kinds=all --suppressions=./push_rl.supp --trace-children=yes --track-origins=yes --track-fds=yes -s ./minishell
