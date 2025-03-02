4e5289703f5313766d3261ff66c3c0c9d1a3b264
alex tambo <alex.tambo.15432@gmail.com>
2025-03-02 12:03:32 +0100

    Mod push_test now not just huge line also added log file so exec and log text is separate

--- push_test.sh
+++ push_test.sh
@@ -1,2 +1,11 @@
-clear && make re && clear && valgrind --leak-check=full --show-leak-kinds=all --suppressions=./push_rl.supp --trace-children=yes --track-origins=yes --track-fds=yes -s ./minishell
-
+clear && \
+make re && \
+clear && \
+valgrind --leak-check=full \
+         --show-leak-kinds=all \
+		 --log-file=push_valgrind_log.txt \
+         --suppressions=./push_rl.supp \
+         --trace-children=yes \
+         --track-origins=yes \
+         --track-fds=yes \
+         -s ./minishell
