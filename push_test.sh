217fd48c55dba2614753a46ab5623d52f7ad883e
atambo <alex.tambo.15432@gmail.com>
2025-03-04 15:48:37 +0100

    restore push test file

--- push_test.sh
+++ push_test.sh
@@ -5,6 +5,7 @@ clear && \
 valgrind --leak-check=full \
          --show-leak-kinds=all \
 		 --log-file=push_valgrind_log.txt \
+         --suppressions=./push_rl.supp \
          --trace-children=yes \
          --track-origins=yes \
          --track-fds=yes \
