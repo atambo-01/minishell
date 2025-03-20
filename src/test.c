70af440c39aad248d3698b178b9ff0cfa9326252
Eliandra Neto <eneto@c1r4s3.42luanda.com>
2025-03-20 22:18:35 +0100

    Norma in aux_list.c finished

--- src/test.c
+++ src/test.c
@@ -1,48 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   aux_env_list.c                                     :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eneto <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/16 18:15:47 by eneto             #+#    #+#             */
-/*   Updated: 2025/03/20 12:35:42 by eneto            ###   ########.fr       */
+/*   Created: 2025/03/20 12:00:26 by eneto             #+#    #+#             */
+/*   Updated: 2025/03/20 12:00:58 by eneto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "../inc/minishell.h"
-
-char	*ft_getenv(t_env *env, const char *name)
-{
-	if (!env)
-		return (NULL);
-	while (env)
-	{
-		if (ft_strcmp(env->name, name) == 0)
-		{
-			if (env->value)
-				return (env->value);
-			else
-				return ("no name or =");
-		}
-		env = env->next;
-	}
-	return (NULL);
-}
-
-void	ft_aux(t_env **head, t_env **new, t_env **cur)
-{
-	if (!(*head))
-	{
-		*head = *new;
-		*cur = *head;
-	}
-	else
-	{
-		(*cur)->next = *new;
-		*cur = *new;
-	}
-}
 
 char	**ft_token_to_envp(t_env *env)
 {
