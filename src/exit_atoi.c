12da24a1b6166f3e9fe950a442dbfd376880e752
atambo <alex.tambo.15432@gmail.com>
2025-03-20 03:14:51 +0100

    Fixed issues on exit, works fine now

--- src/exit_atoi.c
+++ src/exit_atoi.c
@@ -1,12 +1,12 @@
-/* ************************************************************************** */
 /*                                                                            */
+/* ************************************************************************** */
 /*                                                        :::      ::::::::   */
 /*   exit_atoi.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: atambo <alex.tambo.15432@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 14:04:46 by atambo            #+#    #+#             */
-/*   Updated: 2025/03/18 11:33:42 by atambo           ###   ########.fr       */
+/*   Updated: 2025/03/20 02:59:16 by atambo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,30 +21,16 @@ int	ft_convert(long long l)
 	return ((int)l);
 }
 
-int	handle_space_and_signs(char *str, t_count *c)
+void	handle_sign(char *str, t_count *c)
 {
-	if (str[c->i] == ' ' || str[c->i] == '\t')
+	if (str[c->i] == '-' || str[c->i] == '+')
 	{
-		while (str[c->i] == ' ' || str[c->i] == '\t')
-			c->i++;
-		if (str[c->i + 1])
-		{
-			if (str[c->i] == 0)
-				return(1);
-		}
-	}
-	else if (str[c->i] == '-' || str[c->i] == '+')
-	{
-		if (c->j != 1)
-			return (-1);
-		c->j = 2;
+		c->j = 1;
 		if (str[c->i] == '-')
-			c->j = -2;
+			c->j = -1;
 		c->i++;
 	}
-	else
-		return (0);
-	return (1);
+	return ;
 }
 
 int	handle_quotes(char *str, t_count *c)
@@ -56,60 +42,94 @@ int	handle_quotes(char *str, t_count *c)
 		else if (c->k == str[c->i])
 			c->k = 0;
 		else
-			return (-1);
+			return (1);
 		(c->i)++;
-	}
-	else
-		return (handle_space_and_signs(str, c));
-	return (1);
-}
-
-int	handle_number(char *str, t_count *c)
-{
-	if (isdigit(str[c->i]))
-	{
-		c->l = c->l * 10 + (str[c->i] - '0');
-		if (c->l > 9223372036854775806)
-			return (-1);
-		c->i++;
-		return (1);
-	}
+	} 
 	return (0);
 }
 
-int 	ft_has_digit(char *str)
+int	is_quote(char c)
 {
+	if (c == '"' || c == '\'')
+		return (1);
+	return(0);
+}
+
+int	is_space(char c)
+{
+	if (c == ' '|| c == '\t')
+		return (1);
+	return (0);
+}
+
+int	handle_numbers(char *str, t_count *c)
+{
+	while (ft_isdigit(str[c->i]) || is_quote(str[c->i]))
+	{
+		if (is_quote(str[c->i]))
+		{
+			if (handle_quotes(str, c) != 0)
+				return (-1);
+			continue;
+		}
+		c->l = c->l * 10 + (str[c->i] - '0');
+		(c->i)++;
+	}
+	while (is_quote(str[c->i]) || is_space(str[c->i]))
+	{
+		if (handle_quotes(str, c) != 0)
+			return (-1);
+		else if (is_space(str[c->i]))
+			(c->i)++;
+	}
+	if (str[c->i] != 0)
+		return (-1);
+	return (0);
+}
+
+int 	ft_check_number(char *str)
+{
+	int		i;
+	char	*sign;
+
+	i = 0;
+	sign = ft_strchr(str, '-');
 	while(*str)
 	{
-		if (*str >= '0' && *str <= '9')
-			return (1);
+		if (ft_isdigit(*str))
+		{	i++;
+			if (i > ft_strlen("9223372036854775807"))
+				return (-1);
+			if (i == ft_strlen("9223372036854775807"))
+			{	
+				if (sign == NULL && *str > '7')
+					return (-1);
+				if (sign != NULL && *str > '8')
+					return (-1);
+			}
+		}	
 		str++;
 	}
-	return(0);
+	return(i);
 }
 
 int ft_exit_atoi(char *str)
 {
-	int		processed;
 	t_count	c;
 
 	ft_counter(&c);
-	if (!str || !*str || ft_has_digit(str) == 0)
+	if (!str || !*str || ft_check_number(str) <= 0)
 		return (-1);
 	c.j = 1;
-	while (str[c.i])
+	while (is_quote(str[c.i]) || is_space(str[c.i]))
 	{
-		processed = handle_quotes(str, &c);
-		if (processed == -1)
+		if (handle_quotes(str, &c) != 0)
 			return (-1);
-		if (processed == 0) 
-		{
-			processed = handle_number(str, &c);
-			if (processed == -1)
-				return (-1);
-			if (processed == 0)
-				return (-1);
-		}
+		else if (is_space(str[c.i]))
+			(c.i)++;
 	}
+	handle_sign(str, &c);
+	if (handle_numbers(str, &c) != 0)
+		return (-1);
 	return (ft_convert((c.j / abs(c.j)) * c.l));
 }
